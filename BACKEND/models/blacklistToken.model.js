ca811d891e006067c0195c0a7858a60b290c5c89
Sahil Bhingardive <sahilbhingardive72@gmail.com>
2025-10-08 07:34:30 +0530

    fix: corrected blacklistToken imports and backend file updates

--- BACKEND/models/blacklistToken.model.js
+++ BACKEND/models/blacklistToken.model.js
@@ -13,4 +13,4 @@ const blacklistTokenSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
+module.exports = mongoose.model('blacklistToken', blacklistTokenSchema);
